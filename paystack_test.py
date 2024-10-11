a004fa5c7203030f26585b43290b134bd010436b
Agboola Olamidipupo <dipoagboola2019@gmail.com>
2024-10-11 01:18:38 +0100

    updtae: beefed the paystack service slighhtly

---
+++ paystack_test.py
@@ -0,0 +1,18 @@
+import paystack
+
+ctx=paystack.Paystack()
+
+"""
+initialize transaction
+
+"""
+# print(ctx.initialize_transaction("dipoagboola2019@gmail.com",200))
+
+
+
+"""
+
+confirm transaction
+
+"""
+# print(ctx.confirm_transaction("tjkdiy1rg1i7esh"))
